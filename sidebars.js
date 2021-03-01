7364e056c2733300b63d1cc9f89b7e058a330497
Tom Hacohen <tom@stosb.com>
2021-03-01 10:22:58 +0200

    Add a section about consuming webhooks.

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,12 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  someSidebar: [
+    'introduction',
+    {
+      'Getting Started': ['doc1', 'doc2', 'doc3'],
+      Features: ['mdx'],
+      'Consuming Webhooks': [
+        'receiving/verifying-payloads',
+      ],
+    },
+  ],
 };
