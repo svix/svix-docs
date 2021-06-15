2235ef7a4f931ef37782b9a3cd36fbf894f4583b
Frank Chiarulli Jr <frank@svix.com>
2021-06-15 15:43:42 -0400

    Add CLI tutorial

--- sidebars.js
+++ sidebars.js
@@ -5,6 +5,9 @@ module.exports = {
       'Getting Started': ['overview', 'installation', 'quickstart', 'event-types', 'management-ui'],
     },
     'consuming-webhooks',
+    {
+      'Tutorials': [ 'cli' ],
+    },
     'get-help',
   ],
   consumersSidebar: [
