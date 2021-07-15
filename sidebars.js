5d0478c37103e8b93052205a5d9f77ac9ed0f925
Tom Hacohen <tom@stosb.com>
2021-07-15 21:29:29 +0300

    Document the retry schedule.

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     'introduction',
     {
       'Getting Started': ['overview', 'installation', 'quickstart', 'event-types', 'management-ui'],
-      'Manage Your Account': ['account/overview', 'account/environments'],
+      'Manage Your Account': ['account/overview', 'account/environments', 'account/retries'],
     },
     'consuming-webhooks',
     {
