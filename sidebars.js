8d392edcc4ca51b76f51f153f9d1f539bf01e1fd
Liam Cain <liam@svix.com>
2021-07-25 21:33:35 -0400

    Improve App Portal documentation
    - Finish renaming Management UI to App Portal
    - Improve the default styling for the embedded App Portal

--- sidebars.js
+++ sidebars.js
@@ -1,40 +1,57 @@
 module.exports = {
   mainSidebar: [
     {
-      'Introduction': ['introduction', 'overview', 'installation', 'quickstart', 'event-types', 'management-ui', 'consuming-webhooks'],
-      'Advanced': ['incoming-webhooks', 'rate-limit'],
-      'Manage Your Account': ['account/overview', 'account/environments', 'account/org-members', 'account/retries'],
+      Introduction: [
+        "introduction",
+        "overview",
+        "installation",
+        "quickstart",
+        "event-types",
+        "app-portal",
+        "consuming-webhooks",
+      ],
+      Advanced: ["incoming-webhooks", "rate-limit"],
+      "Manage Your Account": [
+        "account/overview",
+        "account/environments",
+        "account/org-members",
+        "account/retries",
+      ],
     },
     {
-      'Tutorials': [ 'tutorials/cli' ],
+      Tutorials: ["tutorials/cli"],
     },
     {
-      'Additional links': [
-        'play',
-        'get-help',
+      "Additional links": [
+        "play",
+        "get-help",
         {
-          type: 'link',
-          href: 'https://api.svix.com/docs',
-          label: 'API Reference',
+          type: "link",
+          href: "https://api.svix.com/docs",
+          label: "API Reference",
         },
-      ]
+      ],
     },
   ],
   consumersSidebar: [
-    'receiving/introduction',
+    "receiving/introduction",
     {
-      'Verifying Webhooks': ['receiving/verifying-payloads/why', 'receiving/verifying-payloads/how',  'receiving/verifying-payloads/how-manual'],
-      'Additional links': [
+      "Verifying Webhooks": [
+        "receiving/verifying-payloads/why",
+        "receiving/verifying-payloads/how",
+        "receiving/verifying-payloads/how-manual",
+      ],
+      "Additional links": [
         {
-          type: 'ref',
-          id: 'introduction',
-          label: 'Sending Webhooks',
+          type: "ref",
+          id: "introduction",
+          label: "Sending Webhooks",
         },
         {
-          type: 'ref',
-          id: 'get-help',
+          type: "ref",
+          id: "get-help",
         },
-      ]
+      ],
     },
   ],
 };
