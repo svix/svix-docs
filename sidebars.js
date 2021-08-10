806c8c9b77f1edaf5c75134813092d09ee4e65b3
Tom Hacohen <tom@stosb.com>
2021-08-10 20:47:10 +0300

    Sidebar: make the design flat + other improvements.

--- sidebars.js
+++ sidebars.js
@@ -5,31 +5,37 @@ module.exports = {
       'Advanced': ['rate-limit'],
       'Manage Your Account': ['account/overview', 'account/environments', 'account/org-members', 'account/retries'],
     },
-    'consuming-webhooks',
     {
       'Tutorials': [ 'tutorials/cli' ],
     },
-    'play',
-    'get-help',
     {
-      type: 'link',
-      href: 'https://api.svix.com/docs',
-      label: 'API Reference',
+      'Additional links': [
+        'consuming-webhooks',
+        'play',
+        'get-help',
+        {
+          type: 'link',
+          href: 'https://api.svix.com/docs',
+          label: 'API Reference',
+        },
+      ]
     },
   ],
   consumersSidebar: [
     'receiving/introduction',
     {
       'Verifying Webhooks': ['receiving/verifying-payloads/why', 'receiving/verifying-payloads/how',  'receiving/verifying-payloads/how-manual'],
-    },
-    {
-      type: 'ref',
-      id: 'introduction',
-      label: 'Sending Webhooks',
-    },
-    {
-      type: 'ref',
-      id: 'get-help',
+      'Additional links': [
+        {
+          type: 'ref',
+          id: 'introduction',
+          label: 'Sending Webhooks',
+        },
+        {
+          type: 'ref',
+          id: 'get-help',
+        },
+      ]
     },
   ],
 };
