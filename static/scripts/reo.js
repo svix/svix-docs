e6dde193c8129e7673e2eec2990500c0c9dfe790
svix-ken <102979300+svix-ken@users.noreply.github.com>
2024-05-20 16:45:48 -0400

    add reo (#154)

---
+++ static/scripts/reo.js
@@ -0,0 +1,3 @@
+//Start of Reo Javascript
+!function(){var e,t,n;e="d4c659ca4e6f12a",t=function(){Reo.init({clientID:"d4c659ca4e6f12a"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.async=!0,n.onload=t,document.head.appendChild(n)}();
+//End of Reo Javascript
