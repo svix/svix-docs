351450380b4f614b15d401fd862183d1e3cb9317
svix-lucho <lucho@svix.com>
2025-12-31 10:43:58 -0300

    Rename endpoint rate limiting to throttling

--- sidebars.js
+++ sidebars.js
@@ -11,7 +11,7 @@ module.exports = {
       ],
       "Advanced Functionality": [
         "idempotency",
-        "rate-limit",
+        "throttling",
         "transformations",
         {
           type: 'category',
