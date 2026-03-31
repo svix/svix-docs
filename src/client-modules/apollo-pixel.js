a2fbc6803b5e7b1d3108b7a44558777bb705133f
svix-lucho <lucho@svix.com>
2026-03-31 15:58:34 -0300

    remove consolelog

--- src/client-modules/apollo-pixel.js
+++ src/client-modules/apollo-pixel.js
@@ -2,7 +2,6 @@ const APOLLO_APP_ID = '66317583955bbd0438bc78c3';
 
 function trackApolloPage() {
   if (window.trackingFunctions) {
-    console.log('trackingFunctions', window.trackingFunctions);
     window.trackingFunctions.onLoad({ appId: APOLLO_APP_ID });
   }
 }
