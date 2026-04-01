899cd59f9fbad00aba2fb43d8ea4dcea620299df
svix-lucho <133019767+svix-lucho@users.noreply.github.com>
2026-04-01 08:57:54 -0300

    Fix apollo build error (#267)

--- src/client-modules/apollo-pixel.js
+++ src/client-modules/apollo-pixel.js
@@ -1,23 +1,13 @@
 const APOLLO_APP_ID = '66317583955bbd0438bc78c3';
 
-function loadApolloScript() {
-  const nocache = Math.random().toString(36).substring(7);
-  const script = document.createElement('script');
-  script.src = `https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=${nocache}`;
-  script.async = true;
-  script.defer = true;
-  script.onload = function () {
+function trackApolloPage() {
+  if (window.trackingFunctions) {
     window.trackingFunctions.onLoad({ appId: APOLLO_APP_ID });
-  };
-  document.head.appendChild(script);
+  }
 }
-
-loadApolloScript();
 
 export function onRouteDidUpdate({ location, previousLocation }) {
   if (location.pathname !== previousLocation?.pathname) {
-    if (window.trackingFunctions) {
-      window.trackingFunctions.onLoad({ appId: APOLLO_APP_ID });
-    }
+    trackApolloPage();
   }
 }
---
+++ static/js/apollo.js
@@ -0,0 +1,11 @@
+(function () {
+  var APOLLO_APP_ID = '66317583955bbd0438bc78c3';
+
+  var script = document.createElement('script');
+  script.id = 'apollo-tracker-script';
+  script.src = 'https://assets.apollo.io/micro/website-tracker/tracker.iife.js';
+  script.async = true;
+  script.defer = true;
+
+  document.head.appendChild(script);
+})();
