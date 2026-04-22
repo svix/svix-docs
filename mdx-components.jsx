9476372255d26af37557b3c765515339be1574d5
svix-lucho <lucho@svix.com>
2026-04-22 11:41:28 -0300

    One more fixx

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -1,10 +1,21 @@
 import { Callout } from 'nextra/components'
-import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs' // nextra-theme-blog or your custom theme
+import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
 import { CodeTab, CodeTabGroup } from './src/components/CodeTabs'
 import { CLITabs } from './src/components/CLITabs'
 import ConnectorLogo from './src/components/ConnectorLogo'
+import clsx from 'clsx'
+import Image from 'next/image'
 
-const themeComponents = getThemeComponents()
+const themeComponents = getThemeComponents({
+  img: function DocsImage({ className, ...props }) {
+    return (
+      <Image
+        className={clsx('dark:bg-slate-50 rounded-md shadow-xs', className)}
+        {...props}
+      />
+    )
+  },
+})
 
 // Merge components
 export function useMDXComponents(components) {
