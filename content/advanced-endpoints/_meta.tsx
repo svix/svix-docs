770fe4c6e490872efc384f1e9c64700aa9716012
Gabriel <gabriel@svix.com>
2026-07-10 11:45:09 -0400

    Add Advanced Endpoint docs for missing advanced endpoints

--- content/advanced-endpoints/_meta.tsx
+++ content/advanced-endpoints/_meta.tsx
@@ -4,6 +4,15 @@ const meta = {
   'polling-endpoints': 'Polling Endpoints',
   'fifo-endpoints': 'FIFO Endpoints',
   'object-storage': 'Object Storage',
+  'otel-tracing': 'OpenTelemetry Tracing',
+  snowflake: 'Snowflake',
+  rabbitmq: 'RabbitMQ',
+  eventbridge: 'Amazon EventBridge',
+  sns: 'Amazon SNS',
+  sqs: 'Amazon SQS',
+  bigquery: 'Google BigQuery',
+  clickhouse: 'ClickHouse',
+  redshift: 'Amazon Redshift',
 } satisfies Meta
 
 export default meta
